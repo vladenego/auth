f11c26de8a3795c1370f8a860b2371a59aa697f0
vladenego <mistervlad3@gmail.com>
2020-05-06 21:26:37 +0300

    Added hash logic for passwords

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 var express = require('express')
+const bcrypt = require('bcrypt');
 var router = express.Router()
 const {registerValidation} = require('../validation')
 
@@ -13,16 +14,22 @@ router.post('/register', async (req,res) => {
   if(error) return res.status(400).send(error.details[0].message)
 
   //CHECK IF THE USER IS ALREADY IN DARABASE BY USERNAME
+  const usernameExist = await User.findOne({username: req.body.username})
+  if (usernameExist) return res.status(400).send('this username is already exist')
 
   // CHECK IF THE USER IS ALREADY IN DATABASE
   const emailExist = await User.findOne({email: req.body.email})
   if (emailExist) return res.status(400).send('this email is already exist')
 
+
+  // HASH THE PASSWORD
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
   //CREATE A NEW USER
   const user = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassword
   })
 
   try{
